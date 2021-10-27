384bc10b02524f04c6ed964052d68fe6816c92ec
Artus U <artusrock@hotmail.com>
2021-10-27 11:42:15 +0100

    Main.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+import Details from "./components/Details/Details";
+import Main from "./components/Main/Main";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4} className={classes.mobile}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.main}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
