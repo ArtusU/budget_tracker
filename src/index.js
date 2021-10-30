ee50325802ab8f6c162339ca21cd17bf262160a7
Artus U <artusrock@hotmail.com>
2021-10-30 12:06:59 +0100

    data from speechly segment.intent.intent

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <SpeechProvider appId="" language="en-US">
+  <SpeechProvider appId="5feca692-d07c-45af-b717-078d51518721" language="en-US">
     <Provider>
       <App />
     </Provider>
