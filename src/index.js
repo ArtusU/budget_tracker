d1b135af0e4cc306dde34ff3b1990458158b9337
Artus U <artusrock@hotmail.com>
2021-10-30 16:37:08 +0100

    .env.info

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SpeechProvider } from '@speechly/react-client';
+import React from "react";
+import ReactDOM from "react-dom";
+import { SpeechProvider } from "@speechly/react-client";
 
-import { Provider } from './context/context';
-import App from './App';
-import './index.css';
+import { Provider } from "./context/context";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
-  <SpeechProvider appId="5feca692-d07c-45af-b717-078d51518721" language="en-US">
+  <SpeechProvider appId={ process.env.REACT_APP_SPEECHLY_ID } language="en-US">
     <Provider>
       <App />
     </Provider>
   </SpeechProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
